68df7e39c28f5760fc1d250f0bee7d59278e968b
Cristian Prochnow <cristianprochnow@gmail.com>
2024-02-29 23:15:47 -0300

    adicionar a tipagem do model e também alguns exemplos de dados

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
 package main
+
+type car struct {
+	Id    int     `json:"id"`
+	Model string  `json:"model"`
+	Brand string  `json:"brand"`
+	Price float64 `json:"price"`
+}
+
+var cars = []car{
+	{
+		Id:    1,
+		Brand: "Honda",
+		Model: "Civic",
+		Price: 164000.5,
+	},
+	{
+		Id:    2,
+		Brand: "Toyota",
+		Model: "Corola",
+		Price: 145000.8,
+	},
+	{
+		Id:    3,
+		Brand: "Kia",
+		Model: "Cerato",
+		Price: 123500.5,
+	},
+	{
+		Id:    4,
+		Brand: "Hyundai",
+		Model: "Elantra",
+		Price: 118560.5,
+	},
+}
